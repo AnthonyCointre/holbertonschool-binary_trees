fbe20c48c723bc7049cda67d2690cd0bdf9267c1
AnthonyCointre <8732@holbertonstudents.com>
2024-04-10 12:35:29 +0200

    creation 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -11,10 +11,14 @@
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+	{
 		return (NULL);
+	}
 
 	if (node->parent->parent->right == node->parent)
+	{
 		return (node->parent->parent->left);
+	}
 
 	return (node->parent->parent->right);
 }
